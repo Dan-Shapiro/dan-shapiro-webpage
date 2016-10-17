b77f98539558d5cf13a911321e5f4142425cdb44
Dan <ds5ct@virginia.edu>
2016-10-17 01:35:27 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,114 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+//TABS START - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+var Pane = React.createClass({
+  render: function() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {this.props.children}
       </div>
     );
   }
-}
+});
+
+var Tabs = React.createClass({
+  getDefaultProps: function() {
+    return {
+      selected: 0
+    };
+  },
+
+  getInitialState: function() {
+    return {
+      selected: this.props.selected
+    };
+  },
+
+  handleClick: function(index, event) {
+    event.preventDefault();
+    this.setState({
+      selected: index
+    });
+  },
+
+  createTitles: function() {
+    function titles(child, index) {
+      let activeClass = (this.state.selected === index ? 'active' : '');
+      return (
+        <li key={index}>
+          <a href='#' className={activeClass} onClick={this.handleClick.bind(this, index)}>
+            {child.props.title}
+          </a>
+        </li>
+      );
+    }
+    return (
+      <ul className='tabs-titles'>
+        {this.props.children.map(titles.bind(this))}
+      </ul>
+    );
+  },
+
+  createContent: function() {
+    return (
+      <div className='tabs-content'>
+        {this.props.children[this.state.selected]}
+      </div>
+    );
+  },
+
+  render: function() {
+    return (
+      <div className='tabs'>
+        {this.createTitles()}
+        {this.createContent()}
+      </div>
+    );
+  }
+});
+//TABS END- - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+var ResumeContent = React.createClass({
+  render: function() {
+    return (
+      <a href='./resume/Dan_Shapiro_Resume.pdf' download>Download</a>
+    );
+  }
+});
+
+//APP START- - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <Tabs selected={0}>
+          <Pane title="Home">
+            <div>
+              <p>This is my Home content!</p>
+            </div>
+          </Pane>
+          <Pane title="Academic">
+            <div>
+              <p>This is my Academic content!</p>
+            </div>
+          </Pane>
+          <Pane title="Personal">
+            <div>
+              <p>This is my Personal content!</p>
+            </div>
+          </Pane>
+          <Pane title="Resume">
+            <div>
+              <p>This is my Resume content!</p>
+              <ResumeContent />
+            </div>
+          </Pane>
+        </Tabs>
+      </div>
+    );
+  }
+});
+//APP END- - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 export default App;
